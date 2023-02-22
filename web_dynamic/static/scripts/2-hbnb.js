eebcb8aa1884c716231b8ad56ad9a046400cbea1
Ousman <usman.traid@gmail.com>
2023-02-22 03:52:55 -0500

    firs three tasks completed

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,19 @@
+$(function () {
+  amenities = {};
+  $(".popover input").change(function () {
+    if ($(this).is(":checked")) {
+      amenities[$(this).attr("data-id")] = $(this).attr("data-name");
+    } else {
+      delete amenities[$(this).attr("data-id")];
+    }
+    $(".amenities H4").text(Object.values(amenities).join(", "));
+  });
+
+  $.get("http://127.0.0.1:5001/api/v1/status/", function (data) {
+    if (data.status === "OK") {
+      $("#api_status").addClass("available")
+    } else {
+      $("#api_status").removeClass("available")
+    }
+  });
+});
